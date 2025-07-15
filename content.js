5f2c8f9edcaec2e9c0b4339b6a0e9c1db1a0989b
Israel <g9931526@gmail.com>
2025-07-15 14:23:46 +0300

    Refactor input handling and update content script match
    Refactored input logic by introducing a debounced save to Chrome storage and improved code structure for element interaction. Updated manifest to match all paths under the sso.apps.openu.ac.il domain, ensuring the content script runs on more relevant pages.

--- content.js
+++ content.js
@@ -1,28 +1,38 @@
-function clickElement() {
-  const element = document.getElementById("p_mis_student");
-  if (element) {
-    element.focus();
-    chrome.storage.local.get('data', function (result) {
-      if (result.data) {
-        element.value = result.data;
-        element.click();
-      }
-    });
-    element.addEventListener('input', function () {
-      const userInput = element.value;
-      chrome.storage.local.set({ data: userInput }, function () {
-      });
-    });
-  }
-  else {
-    clickElement()
-  }
-  // const element1 = document.querySelector('[type=submit]');
-  // const element1 = document.getElementById("login_sso");
-  // console.log(element1);
-  // setTimeout(function() {
-  //   element1.submit();
-  // }, 2000);
+function debounce(func, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, delay);
+  };
 }
 
-clickElement();
+function clickElement(element) {
+  element.focus();
+  chrome.storage.local.get('data', function (result) {
+    if (result.data) {
+      element.value = result.data;
+      element.click();
+    }
+  });
+
+  const saveData = function () {
+    const userInput = element.value;
+    chrome.storage.local.set({ data: userInput });
+  };
+
+  const debouncedSaveData = debounce(saveData, 500); // Debounce for 500ms
+
+  element.addEventListener('input', debouncedSaveData);
+}
+
+function autoInput() {
+  const element = document.getElementById("p_mis_student");
+  if (element) {
+    clickElement(element);
+  }
+  else {
+    setTimeout(() => { clickElement(element) }, 2000);
+  }
+}
+
+autoInput();
